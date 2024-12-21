dfaf0bae6f4e05401d79500343e9c875d713bd7b
Ananna-ahmd <157790634+Ananna-ahmd@users.noreply.github.com>
2024-12-21 13:16:33 +0600

    Add files via upload

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,34 @@
+@extends('layout')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>Student Manage</title>
+    <style>
+    body{
+
+    background-image: url('images/desk-workspace-with-various-elements.jpg'); /* Relative path */
+    background-size: cover; /* Ensures the image covers the entire screen */
+    background-repeat: no-repeat;
+    background-attachment: fixed; /* Optional for a parallax effect */
+    }
+    header{
+        text-align: center;
+        font-size: 50px;
+        font-weight:bold;
+
+
+
+
+
+    }
+
+    </style>
+</head>
+<body>
+<header> XYZ School</header>
+</body>
+</html>
